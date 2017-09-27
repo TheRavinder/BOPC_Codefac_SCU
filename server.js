3335a3f4801a0eaa00e8d6ff3320ab275e1dac0e
TheRavinder <ravidhillon1394@icloud.com>
2017-09-28 08:18:10 +1000

    fi00

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.all('/', function(req, res, next) {
 app.use(express.static(__dirname +'/dist/'));
 app.use(require('./routes/api'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join( __dirname +'/dist/index.html'));
-  });
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 app.use(history());
 
