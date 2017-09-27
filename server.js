904d0b67f47e2c8f8f042d24f5949ac82cc0a07d
TheRavinder <ravidhillon1394@icloud.com>
2017-09-28 07:13:36 +1000

    fi02

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var db = require('./lib/db');
+var history = require('connect-history-api-fallback');
+
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -19,6 +21,9 @@ app.all('/', function(req, res, next) {
 });
 app.use(express.static(__dirname +'/dist/'));
 app.use(require('./routes/api'));
+
+
+app.use(history());
 
 
 
