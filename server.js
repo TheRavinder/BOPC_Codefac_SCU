aa2503e9448088fa6c885ab27861141bd74db255
TheRavinder <ravidhillon1394@icloud.com>
2017-09-28 00:13:14 +1000

    final0

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var db = require('./lib/db');
 
+var DIST_DIR = path.join(__dirname, "dist");
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
@@ -17,9 +18,12 @@ app.all('/', function(req, res, next) {
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
-app.use(express.static(__dirname +'/dist/'));
+app.use(express.static(DIST_DIR));
 app.use(require('./routes/api'));
 
+app.get("*", function (req, res) {  
+  res.sendFile(path.join(DIST_DIR, "index.html"));
+});
 
 
 var port = process.env.PORT || 3000;
